b0d2fe785b38c127bb2831bdca3d51b9afa0e8cc
Md Darudul Islam <darudulislam@gmail.com>
2022-12-09 12:53:01 +0800

    conditional rendering with if else

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -23,14 +23,19 @@ class Clock extends React.Component {
         })
     }
     render() {
-        console.log("clock component rendered")
-        const { date, locale } = this.state
+        const { date, locale } = this.state;
+        let button;
+        if (locale === "bn-BD") {
+            button = <Button change={this.handleClick} locale="en-US">Click Me</Button>
+        } else {
+            button = <Button change={this.handleClick} locale="bn-BD">Click Me</Button>
+        }
         return (
             <div>
                 <h1 className='heading'>
                     <span className='text'>{date.toLocaleTimeString(locale)}</span>
                 </h1>
-                <Button change={this.handleClick} locale="en-US">Click Me</Button>
+                {button}
             </div>
 
         )
