5362d5d737c99a09b7478aa62ab81b1495585388
Md Darudul Islam <darudulislam@gmail.com>
2022-12-16 09:58:28 +0800

    Main branch modify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import React from 'react'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <h1>Hello World</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
