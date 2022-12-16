15920d3542f9ba2fe1c2bebbf14afd83d5c93ff4
Md Darudul Islam <darudulislam@gmail.com>
2022-12-16 10:43:12 +0800

    counter with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from 'react'
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      {/* <ClassCounter /> */}
+      <HookCounter />
     </div>
   )
 }
